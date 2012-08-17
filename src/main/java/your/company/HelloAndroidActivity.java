7b07627d7a3c115e984322ce4c59edf1a98eec4c
nicolaquartieri <nicolas.quartieri@gmail.com>
2012-08-17 19:52:30 -0300

    Modificaciones al AndroidManifest.xml y al Main

--- src/main/java/your/company/HelloAndroidActivity.java
+++ src/main/java/your/company/HelloAndroidActivity.java
@@ -1,24 +1,17 @@
 package your.company;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
+import org.apache.cordova.DroidGap;
 
-public class HelloAndroidActivity extends Activity {
+import android.os.Bundle;
+
+public class HelloAndroidActivity extends DroidGap {
 
     private static String TAG = "cordova-primer-app";
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 
 }
